fa8050e8869c7aff52e399c46c83aa0b5c895bd2
Luan <luansfmarques@gmail.com>
2025-02-24 16:07:19 -0300

    create dispersion topic

---
+++ dispersion/quantile_test.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+
+data = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 78, 110]
+
+arr = pd.Series(data)
+
+print(f"Range: {arr.max() - arr.min()}")
+
+print(f"Q1: {arr.quantile(1/4)}")
+print(f"Q2: {arr.quantile(2/4)}")
+print(f"Q3: {arr.quantile(3/4)}")
+print(f"Q4: {arr.max()}")
+
+inter_quantile = arr.quantile(3/4) - arr.quantile(1/4)
+print(f"\nInter Quartile: {inter_quantile}")
+lower_bound = arr.quantile(0.25) - (1.5 * inter_quantile)
+print(f"Lower Bound (to outlier): {lower_bound}")
+upper_bound = arr.quantile(0.75) + (1.5 * inter_quantile)
+print(f"Upper Bound (to outlier): {upper_bound}") # IMPORTANT TO DETECT OUTLIERS
+
+print(f"\nDeviation: {list((arr - arr.mean())**2)}")
+print(f"Variance: {arr.var()}")
+print(f"Standard Deviation: {arr.std()}")
+
+print(f"\nSummary: \n{arr.describe()}")
---
+++ dispersion/std_test.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data1 = pd.Series([1,1,1,1,1,1,1])
+data2 = pd.Series([1,1,1,1,1,2,3])
+data3 = pd.Series([3,2,1,1,1,1,1])
+data4 = pd.Series([1,1,1,1,1,2,10])
+
+print(data1.std())
+print(data2.std())
+print(data3.std())
+print(data4.std())
+print()
+print(data1.mean())
+print(data2.mean())
+print(data3.mean())
+print(data4.mean())
+print()
+print(data1.median())
+print(data2.median())
+print(data3.median())
+print(data4.median())
+
+print(data4.quantile(0.75) + (1.5 * (data4.quantile(0.75) - data4.quantile(0.25))) )
